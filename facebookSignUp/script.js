4f81eb18096031263a29cfc0b5790ab7e64350d8
Farhan Sakhawat <bc160402723@gmail.com>
2022-02-18 11:58:10 +0500

    added'

--- facebookSignUp/script.js
+++ facebookSignUp/script.js
@@ -1,5 +1,15 @@
 function validateFirstName() {
-    
+  let firstName = document.querySelector(".first-name");
+  let firstNameTooltip = document.querySelector(".first-name-tooltip");
+  if(firstName.value == ''){
+    firstName.style.border = "1px solid red";
+    firstNameTooltip.style.display = "block"
+  } else {
+    console.log("normal behaviour")
+    firstName.style.border = "1px solid #dddfe2";
+    firstNameTooltip.style.display = "none";
+  }
+  
 }
 
 function validateSurName() {}
