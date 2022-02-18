566c2d59af4eb4598802059d01965bbec8e108a8
mudassar <codingbiters@gmail.com>
2022-02-18 16:44:04 +0500

    added validation

--- facebookSignUp/script.js
+++ facebookSignUp/script.js
@@ -5,16 +5,46 @@ function validateFirstName() {
     firstName.style.border = "1px solid red";
     firstNameTooltip.style.display = "block"
   } else {
-    console.log("normal behaviour")
     firstName.style.border = "1px solid #dddfe2";
     firstNameTooltip.style.display = "none";
   }
-  
 }
 
-function validateSurName() {}
+function validateSurName() {
+  let surName = document.querySelector(".sur-name");
+  let surNameTooltip = document.querySelector(".sur-name-tooltip");
+  if(surName.value == ''){
+    surName.style.border = "1px solid red";
+    surNameTooltip.style.display = "block"
+  } else {
+    surName.style.border = "1px solid #dddfe2";
+    surNameTooltip.style.display = "none";
+  }
+}
 
-function validateMobileNo() {}
+function validateMobileNo() {
+  let mobileNum = document.querySelector(".mobile-num");
+  let mobileNumTooltip = document.querySelector(".mobile-num-tooltip");
+  if(mobileNum.value == ''){
+    mobileNum.style.border = "1px solid red";
+    mobileNumTooltip.style.display = "block"
+  } else {
+    mobileNum.style.border = "1px solid #dddfe2";
+    mobileNumTooltip.style.display = "none";
+  }
+}
+
+function validatePassword(){
+  let password = document.querySelector(".password");
+  let passwordTooltip = document.querySelector(".password-tooltip");
+  if(password.value == ''){
+    password.style.border = "1px solid red";
+    passwordTooltip.style.display = "block"
+  } else {
+    password.style.border = "1px solid #dddfe2";
+    passwordTooltip.style.display = "none";
+  }
+}
 
 function validateDateOfBirth() {}
 
@@ -22,9 +52,10 @@ function validateGender() {}
 
 function validateForm() {
   validateFirstName();
-  validateLastName();
+  validateSurName();
   validateMobileNo();
   validateDateOfBirth();
   validateGender();
+  validatePassword();
 }
 
